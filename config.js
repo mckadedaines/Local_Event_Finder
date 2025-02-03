fb3912ed7dadb724e50d4d17d6553d2b1cf0a675
Mckade <mckadedaines1@gmail.com>
2025-02-02 21:38:17 -0700

    remove gitignore

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configuration
+const config = {
+  API_KEY: "72vCrK1Md1oUVmciC0MlTtbg49XOn2J2",
+  BASE_URL: "https://app.ticketmaster.com/discovery/v2",
+};
+
+// Prevent modification of config
+Object.freeze(config);
+
+export default config;
